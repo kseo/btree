fab3e83d86209b7049afa3e6df84b1f11d803c3f
Kwang Yul Seo <kwangyul.seo@gmail.com>
2015-11-16 21:07:29 +0900

    Initial import.

---
+++ example/btree.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+library btree.example;
+
+import 'package:btree/btree.dart';
+
+main() {
+  var awesome = new Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
